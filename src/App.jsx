039e0adf1f4cf308bdae2df8d48be9d53b83fb54
Emilse Arias <emilsearias@gmail.com>
2023-05-24 13:48:42 -0300

    agramos un try catch

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,34 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./assets/logosimpson.png";
 import { Button, Container } from "react-bootstrap";
 import Frase from "./components/Frase";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [personaje, setPersonaje] = useState({});
+
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
+  const consultarAPI = async () => {
+    //realizar una peticion get
+    // https://thesimpsonsquoteapi.glitch.me/quotes
+    try{
+      const respuesta = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
+      const dato = await respuesta.json()
+      console.log(respuesta);
+      console.log(dato[0]);
+      setPersonaje(dato[0])
+    }catch (error){
+      console.log(error)
+    }
+  };
+
   return (
     <>
       <Container className="text-center my-5">
         <img src={logo} alt="Logo de los simpson" className="w-50" />
-       <Frase/>
+        <Frase personaje={personaje} />
         <Button variant="warning">Obtener frase</Button>
       </Container>
     </>
--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -1,19 +1,19 @@
 import { Card } from "react-bootstrap";
 
-const Frase = () => {
+const Frase = ({personaje}) => {
   return (
     <section className="my-5">
-      <h2>Personaje</h2>
+      <h2>{personaje.character}</h2>
       <hr />
-      <img className="mb-3" src="https://cdn.glitch.me/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png" alt="" />
+      <img className="mb-3" src={personaje.image} alt={personaje.character} />
       <Card>
         <Card.Body>
           <figure>
             <blockquote className="blockquote">
-              <p>A well-known quote, contained in a blockquote element.</p>
+              <p>{personaje.quote}</p>
             </blockquote>
             <figcaption className="blockquote-footer">
-              Someone famous in <cite title="Source Title">Source Title</cite>
+            {personaje.character} in <cite title="Source Title">The simpson</cite>
             </figcaption>
           </figure>
         </Card.Body>
