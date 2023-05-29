fe537e9a1be078e404428b1b4c810405ba975e2e
Emilse Arias <emilsearias@gmail.com>
2023-05-29 18:09:33 -0300

    agregamos una imagen opcion de un personaje

--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -5,7 +5,7 @@ const Frase = () => {
     <section className="my-5">
       <h2>Nombre de personaje</h2>
       <hr />
-      <img className="mb-3" src='' alt='' />
+      <img className="mb-3" src='https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185' alt='' />
       <Card>
         <Card.Body>
           <figure>
