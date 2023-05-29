334601597b7b9217a9c7d863bf093dce7d17fc7c
Emilse Arias <emilsearias@gmail.com>
2023-05-29 10:21:29 -0300

    agregamos un spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./assets/logosimpson.png";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Spinner } from "react-bootstrap";
 import Frase from "./components/Frase";
 import { useState, useEffect } from "react";
 
 function App() {
   const [personaje, setPersonaje] = useState({});
+  const [mostrarSpinner, setMostrarSpinner] = useState(true);
 
   useEffect(() => {
     consultarAPI();
@@ -15,23 +16,40 @@ function App() {
   const consultarAPI = async () => {
     //realizar una peticion get
     // https://thesimpsonsquoteapi.glitch.me/quotes
-    try{
-      const respuesta = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
-      const dato = await respuesta.json()
+    try {
+      setMostrarSpinner(true);
+      const respuesta = await fetch(
+        "https://thesimpsonsquoteapi.glitch.me/quotes"
+      );
+      const dato = await respuesta.json();
       console.log(respuesta);
       console.log(dato[0]);
-      setPersonaje(dato[0])
-    }catch (error){
-      console.log(error)
+      setPersonaje(dato[0]);
+      //ocultar el spinner
+      setTimeout(() => {
+        setMostrarSpinner(false);
+      }, 2500);
+    } catch (error) {
+      console.log(error);
     }
   };
+
+  const mostrarComponente = mostrarSpinner ? (
+    <div className="my-5">
+      <Spinner variant="primary"> </Spinner>
+    </div>
+  ) : (
+    <Frase personaje={personaje} />
+  );
 
   return (
     <>
       <Container className="text-center my-5">
         <img src={logo} alt="Logo de los simpson" className="w-50" />
-        <Frase personaje={personaje} />
-        <Button variant="warning" onClick={consultarAPI}>Obtener frase</Button>
+        {mostrarComponente}
+        <Button variant="warning" onClick={consultarAPI}>
+          Obtener frase
+        </Button>
       </Container>
     </>
   );
