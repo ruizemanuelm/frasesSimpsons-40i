a59bd2d47351a90d8eadf47258416904c451f5c9
Emanuel Ruiz <ruizemanuelok@gmail.com>
2023-06-03 23:08:48 -0300

    agregamos la funcion al boton

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ const consultarAPI = async ()=> {
       <Container className="text-center my-5">
         <img src={logo} alt="Logo de los simpson" className="w-50" />
         <Frase personaje={personaje} />
-        <Button variant="warning" >
+        <Button variant="warning" onClick={consultarAPI}>
           Obtener frase
         </Button>
       </Container>
